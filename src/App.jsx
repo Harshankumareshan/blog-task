b9cc41d0e1ce819669e52f28ce9a8c544a81fef8
harshankumareshan <harshank333@gmail.com>
2023-11-10 12:40:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Createblog from './Components/Createblog'
+import ManageBlogs from './Components/ManageBlogs'
+import Home from './Components/Home'
+import EditBlog from './Components/EditBlog'
+
+function App() {
+  
+
+  return (
+    <>
+     <BrowserRouter>
+     <div> 
+      <NavBar/>
+     </div>
+     <div className="container-fluid">
+<Routes>
+<Route path="/create" element={<Createblog/>}/>
+<Route path="/manage" element={<ManageBlogs/>}/>
+<Route path="/edit/:id" element={<EditBlog/>}/>
+<Route path="/*" element={<Home/>}/>
+</Routes>
+     </div>
+
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
